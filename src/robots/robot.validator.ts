3f4180126624b27cf0cfd36e9df110cf84fdf125
Rafael Valencia <rafaelvalencia@ravn.co>
2022-09-18 00:30:07 -0300

    chore: add joi to validate robot data

---
+++ src/robots/robot.validator.ts
@@ -0,0 +1,12 @@
+import * as Joi from 'joi';
+
+const schemas = { 
+  newRobot: Joi.object().keys({
+    name: Joi.string().required(),
+    numberOfArms: Joi.number().required(),
+    model: Joi.string().required(),
+    manufacturedBy: Joi.string()
+  })
+};
+
+export default schemas;
