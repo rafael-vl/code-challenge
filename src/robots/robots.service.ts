91e421b4b69a4618d18e1937041f8c0b7757f88b
Rafael Valencia <rafaelvalencia@ravn.co>
2022-09-18 00:47:09 -0300

    feat: add RobotService

---
+++ src/robots/robots.service.ts
@@ -0,0 +1,21 @@
+import { RobotRepository } from "./robot-repository.interface";
+import { RobotDto } from "./robot.dto";
+
+export class RobotsService {
+
+  readonly robotRepository: RobotRepository;
+
+  constructor(robotRepository: RobotRepository){
+    this.robotRepository = robotRepository;
+  };
+
+  async find(id: number): Promise<RobotDto | null> {
+    return this.robotRepository.find(id);
+  }
+  
+  async create(newRobot: RobotDto): Promise<RobotDto> {
+    return this.robotRepository.create(newRobot);
+  }
+  
+}
+
