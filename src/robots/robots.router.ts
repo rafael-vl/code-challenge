cced5adf99ded6e5288a75dd85460a88ba52a2ad
Rafael Valencia <rafaelvalencia@ravn.co>
2022-09-18 00:50:46 -0300

    feat: add POST / handler to robot router

--- src/robots/robots.router.ts
+++ src/robots/robots.router.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 
+import schemas from "./robot.validator";
+import { Robot } from "../db/models/robot.model";
 import { RobotsService } from "./robots.service";
 import { RobotDto } from "./robot.dto";
 import { RobotRepositoryPostgres } from "./robot-repository-postgres";
@@ -20,4 +22,18 @@ robotsRouter.get("/:id", async (req: Request, res: Response) => {
     });
 });
 
-
+robotsRouter.post("/", async (req: Request, res: Response) => {
+    const robot: Robot = req.body;
+    const result = schemas.newRobot.validate(robot);
+    const { error } = result;
+    const valid = error == null;
+    if (!valid) {
+      res.status(422).json({
+        message: 'Invalid request',
+        data: error.message
+      });
+    } else {
+      const newRobot = await robotService.create(robot);
+      res.status(200).json(newRobot);
+    }
+});
